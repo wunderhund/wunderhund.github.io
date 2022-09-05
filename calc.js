9d6616c12674eaea99f9eab9a12b3cfd99c1906d
Craig A. Butler <wunderhund@gmail.com>
2022-09-05 17:55:02 -0400

    affordability calc

--- calc.js
+++ calc.js
@@ -1,12 +1,48 @@
-// Create listeners
-window.addEventListener('load', addListeners)
+// Configure tabs
+window.addEventListener('load', setupTabs)
 
-// Run initial calculation
+// Create Calc listeners
+window.addEventListener('load', addCalcListeners)
+
+// Create Afford listeners
+window.addEventListener('load', addAffordListeners)
+
+// Run initial calculations
 window.addEventListener('load', Calculate)
+window.addEventListener('load', Afford)
 
-function addListeners(){
+function setupTabs(){
+	const tabButtons = Array.from(document.getElementsByClassName("tab-button"));
+    const tabContents = Array.from(document.getElementsByClassName("tab-content"));
+
+	// add tab listeners
+	tabButtons.forEach(button => {
+		button.addEventListener("click", function (evt) {
+			// pull every element with the same data-type as the tab that was clicked:
+			content = evt.target.dataset.type;
+			
+			// Remove 'active' class from all tabs, then set the clicked one to 'active':
+			tabButtons.forEach(btn => {
+				btn.classList.remove('active');
+				if (btn.dataset.type == evt.target.dataset.type) {
+					btn.classList.add('active');
+				}
+			});
+	
+			// Remove 'active' class from all contents, then set the correct one to 'active':
+			tabContents.forEach(btn => {
+				btn.classList.remove('active');
+				if (btn.dataset.type == evt.target.dataset.type) {
+					btn.classList.add('active');
+				}
+			});
+		});
+	});
+}
+
+function addCalcListeners(){
 	// set loan start date to the first day of the next month:
-	today = new Date();
+	const today = new Date();
 	document.getElementById('startDate').valueAsDate = new Date(today.getFullYear(), today.getMonth()+1, 01);
 
 	const principal = document.getElementById('principal')
@@ -48,10 +84,49 @@ function Calculate() {
 
 	// Populate output
 	outputDiv.innerHTML = output;
+}
 
-	// Change height of main box
-	const outheight = document.getElementById('output').clientHeight;
-	newheight = 550+outheight
-	mainbox = document.getElementById('main-box')
-	mainbox.setAttribute("style",`height:${newheight}px`);
+function addAffordListeners(){
+	const grossIncome = document.getElementById('gross-income')
+	const downPayment = document.getElementById('down-payment')
+	const fico = document.getElementById('fico')
+	const affordAPR = document.getElementById('afford-interest')
+	const affordYears = document.getElementById('afford-years')
+
+	grossIncome.addEventListener('change',Afford);
+	downPayment.addEventListener('change',Afford);
+	fico.addEventListener('change',Afford);
+	affordAPR.addEventListener('change',Afford);
+	affordYears.addEventListener('change',Afford);
+}
+
+function Afford() {
+	const affordOutput = document.getElementById('afford-output')
+	const GI = document.getElementById('gross-income').value / 12
+	const down = document.getElementById('down-payment').value
+	const credit = document.getElementById('fico').value
+	const affordInterest = document.getElementById('afford-interest').value / 100;
+	const affordMonthlyInterest = Number(parseFloat(affordInterest / 12).toPrecision(4));
+	const affordN = document.getElementById('afford-years').value * 12
+
+	const monthlyPayment = GI * 0.28
+	const affordMagic = ((1 + affordMonthlyInterest) ** affordN);
+	const maxMonthly = (monthlyPayment / ((affordMonthlyInterest * affordMagic) / (affordMagic - 1)))
+	const maxDown = down / credit
+	const maxMort = Number(down) + Math.min(maxMonthly,maxDown)
+	
+	// Clear output div
+	affordOutput.innerHTML = "";
+
+	// Build output string
+	const afford = `Total number of payments: ${affordN}` + "<br />" + 
+		"Interest rate: " + Number(affordInterest*100).toFixed(2)+"%" +  "<br />" +
+		`Max Monthly Payment: ${monthlyPayment.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}` + "<br />" + 
+		"Max Mortgage Based on Monthly Payment: " + maxMonthly.toLocaleString('en-US', { style: 'currency', currency: 'USD' }) +  "<br />" + 
+		"Max Mortgage Based on Down Payment: " + maxDown.toLocaleString('en-US', { style: 'currency', currency: 'USD' }) +  "<br />" +
+		"Max House Price I Can Afford: " + maxMort.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+    console.log(afford)
+	
+	// Populate output
+	affordOutput.innerHTML = afford;
 }
